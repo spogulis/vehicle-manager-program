8b32528ede8b29d1ca7d44fead009e625f9afb66
Renārs Grebežs <originalchc@gmail.com>
2018-06-03 12:26:49 +0300

    List btn opens a new window

--- gui.py
+++ gui.py
@@ -1,28 +1,27 @@
 import sys
-from PyQt5.QtWidgets import (QWidget, QGridLayout,
-    QPushButton, QApplication, QLabel)
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
-class MainWindow(QApplication):
+
+class MainWindow(QWidget):
     def __init__(self):
-        super().__init__()
+        super(QWidget, self).__init__()
 
-
-
-class MainMenu(QWidget):
-    def __init__(self):
-        super(MainMenu, self).__init__()
-    def initUI(self):
+        self.setGeometry(300, 300, 350, 300)
+        self.second = SecondWindow()
         #GRID layout
         grid = QGridLayout()
         self.setLayout(grid)
-        grid.setSpacing(10)
+        grid.setSpacing(20)
 
         #Buttons
-        list_all_cars_btn = QPushButton("Show me the cars")
+        list_all_cars_btn = QPushButton("List cars")
         add_car_btn = QPushButton("Add car")
         edit_car_btn = QPushButton("Edit car")
         delete_car_btn = QPushButton("Delete a car")
         quit_btn = QPushButton("Quit")
+        quit_btn.setStatusTip('Quit the program')
 
         #List of labels
         list_all_cars_q = QLabel('Press this button to view all your cars')
@@ -41,17 +40,38 @@ class MainMenu(QWidget):
         grid.addWidget(delete_car_btn, 4, 1)
         grid.addWidget(quit_btn, 5, 2)
 
+        #BUTTON ACTIONS
+        #Quit
+        quit_btn.clicked.connect(QApplication.instance().quit)
 
-        self.setLayout(grid)
+        #Window2
+        list_all_cars_btn.clicked.connect(self.button_clicked)
+
         self.setGeometry(300, 300, 350, 300)
         self.setWindowTitle('Main menu - Vehicle manager')
         self.show()
 
+    def button_clicked(self):
+        self.sw = SecondWindow()
+        self.sw.show()
 
-app = MainWindow()
-main_menu = MainMenu()
 
+class SecondWindow(QWidget):
+    def __init__(self):
+        super(QWidget, self).__init__()
+
+        self.setGeometry(300, 300, 350, 300)
+        #GRID layout
+        grid = QGridLayout()
+        self.setLayout(grid)
+        grid.setSpacing(20)
+        list_all_cars_q = QLabel('Press this button to view all your cars')
+        grid.addWidget(list_all_cars_q, 1, 0)
+        self.show()
+
+
+app = QApplication(sys.argv)
+mw = MainWindow()
 sys.exit(app.exec_())
-
 
 
