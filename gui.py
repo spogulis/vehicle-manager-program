33e34243484761caaededdbefeb2d8e295627dea
Renārs Grebežs <originalchc@gmail.com>
2018-06-12 10:09:56 +0300

    Added GUI option - add and save a vehicle to file.
    "Add car" dialog implemented as hidden QWidgets that toggle upon clicking a button.

--- gui.py
+++ gui.py
@@ -10,63 +10,167 @@ from Vehicle import Vehicle
 class MainWindow(QWidget):
 
     switch_window = QtCore.pyqtSignal()
+    add_car_signal = QtCore.pyqtSignal()
 
     def __init__(self):
         super(QWidget, self).__init__()
-
-        self.setGeometry(300, 300, 350, 300)
         self.setWindowTitle('Vehicle manager')
+        def add_car_dialog(self):
+            def add_car_submit():
+                filename = "list.txt"
+                cars = read_from_file(filename)
+                brand = self.brand_input.text()
+                model = self.model_input.text()
+                km_done = self.mileage_input.text()
+                service_date = self.date_input.text()
+
+                new_car = Vehicle(brand, model, km_done, service_date)
+                cars.append(new_car)
+
+                if len(cars) > 0:
+                    with open(filename, "w+", encoding="utf-8") as file_handle:
+                        for car in cars:
+                            line = car.generate_file_line()
+                            file_handle.write(line)
+                self.brand_input.setText("")
+                self.model_input.setText("")
+                self.mileage_input.setText("")
+                self.date_input.setText("")
+
+            self.brand_label = QLabel("Enter brand name: ")
+            self.brand_input = QLineEdit()
+            self.brand_input.setFixedWidth(400)
+            self.brand_label.setHidden(True)
+            self.brand_input.setHidden(True)
+
+            self.model_label = QLabel("Enter model name: ")
+            self.model_input = QLineEdit()
+            self.model_input.setFixedWidth(400)
+            self.model_label.setHidden(True)
+            self.model_input.setHidden(True)
+
+            self.mileage_label = QLabel("Enter current mileage: ")
+            self.mileage_input = QLineEdit()
+            self.mileage_input.setFixedWidth(400)
+            self.mileage_label.setHidden(True)
+            self.mileage_input.setHidden(True)
+
+            self.date_label = QLabel("Enter last service date: ")
+            self.date_input = QLineEdit()
+            self.date_input.setFixedWidth(400)
+            self.date_label.setHidden(True)
+            self.date_input.setHidden(True)
+
+            self.submit_btn = QPushButton("Submit")
+            self.submit_btn.setHidden(True)
+
+            self.add_layout.addWidget(self.brand_label, 1, 0)
+            self.add_layout.addWidget(self.brand_input, 1, 1)
+            self.add_layout.addWidget(self.model_label, 2, 0)
+            self.add_layout.addWidget(self.model_input, 2, 1)
+            self.add_layout.addWidget(self.mileage_label, 3, 0)
+            self.add_layout.addWidget(self.mileage_input, 3, 1)
+            self.add_layout.addWidget(self.date_label, 4, 0)
+            self.add_layout.addWidget(self.date_input, 4, 1)
+            self.add_layout.addWidget(self.submit_btn, 5, 1)
+
+            self.submit_btn.clicked.connect(add_car_submit)
+        def show_add_car_dialog():
+            if self.add_car_toggled == False:
+                self.brand_label.setHidden(False)
+                self.brand_input.setHidden(False)
+                self.model_label.setHidden(False)
+                self.model_input.setHidden(False)
+                self.mileage_label.setHidden(False)
+                self.mileage_input.setHidden(False)
+                self.date_label.setHidden(False)
+                self.date_input.setHidden(False)
+                self.submit_btn.setHidden(False)
+                self.setFixedHeight(517)
+            elif self.add_car_toggled == True:
+                self.brand_label.setHidden(True)
+                self.brand_input.setHidden(True)
+                self.model_label.setHidden(True)
+                self.model_input.setHidden(True)
+                self.mileage_label.setHidden(True)
+                self.mileage_input.setHidden(True)
+                self.date_label.setHidden(True)
+                self.date_input.setHidden(True)
+                self.submit_btn.setHidden(True)
+                self.setFixedHeight(290)
+            self.add_car_toggled = not self.add_car_toggled
+
+
+
+        # Global car list
+        self.cars = []
+        # Container frame
+        self.main_CW = QWidget()
+
+        # Main layout within container widget
+        self.main_CL = QVBoxLayout(self.main_CW)
+        self.main_CW.setLayout(self.main_CL)
+        self.setFixedWidth(800)
+        self.setFixedHeight(300)
+
+        # Set self layout to main container layout
+        self.setLayout(self.main_CL)
+
         #GRID layout
-        grid = QGridLayout()
-        self.setLayout(grid)
-        grid.setSpacing(20)
+        self.main_grid = QGridLayout()
+        self.main_CL.addLayout(self.main_grid)
+        # self.setLayout(self.main_grid)
+        self.main_CL.addLayout(self.main_grid) # add grid layout to main_CL
+        self.main_grid.setSpacing(20)
+
+        #Is add_car toggled?
+        self.add_car_toggled = False
 
         #Buttons
-        list_all_cars_btn = QPushButton("List cars")
-        add_car_btn = QPushButton("Add car")
-        edit_car_btn = QPushButton("Edit car")
-        delete_car_btn = QPushButton("Delete a car")
-        quit_btn = QPushButton("Quit")
-        quit_btn.setStatusTip('Quit the program')
+        self.list_all_cars_btn = QPushButton("List cars")
+        self.add_car_btn = QPushButton("Add car")
+        self.edit_car_btn = QPushButton("Edit car")
+        self.delete_car_btn = QPushButton("Delete a car")
+        self.quit_btn = QPushButton("Quit")
+        self.quit_btn.setStatusTip('Quit the program')
 
         #List of labels
-        list_all_cars_q = QLabel('Press this button to view all your cars')
-        add_car_q = QLabel('Add a car to your database')
-        edit_car_q = QLabel("Edit a car's record")
-        delete_car_q = QLabel('Delete a car from database')
+        self.list_all_cars_q = QLabel('Press this button to view all your cars')
+        self.add_car_q = QLabel('Add a car to your database')
+        self.edit_car_q = QLabel("Edit a car's record")
+        self.delete_car_q = QLabel('Delete a car from database')
 
         #POSITIONS
-        grid.addWidget(list_all_cars_q, 1, 0)
-        grid.addWidget(list_all_cars_btn, 1, 1)
-        grid.addWidget(add_car_q, 2, 0)
-        grid.addWidget(add_car_btn, 2, 1)
-        grid.addWidget(edit_car_q, 3, 0)
-        grid.addWidget(edit_car_btn, 3, 1)
-        grid.addWidget(delete_car_q, 4, 0)
-        grid.addWidget(delete_car_btn, 4, 1)
-        grid.addWidget(quit_btn, 5, 2)
+        self.main_grid.addWidget(self.list_all_cars_q, 0, 0)
+        self.main_grid.addWidget(self.list_all_cars_btn, 0, 2, 1, 1)
+        self.main_grid.addWidget(self.add_car_q, 1, 0)
+        self.main_grid.addWidget(self.add_car_btn, 1, 2, 1, 1)
+        self.main_grid.addWidget(self.edit_car_q, 2, 0)
+        self.main_grid.addWidget(self.edit_car_btn, 2, 2, 1, 1)
+        self.main_grid.addWidget(self.delete_car_q, 3, 0)
+        self.main_grid.addWidget(self.delete_car_btn, 3, 2, 1, 1)
+        self.main_grid.addWidget(self.quit_btn, 4, 2, 1, 1)
+
+        # Sublayout (add car)
+        self.add_layout = QGridLayout()
+        self.main_CL.addLayout(self.add_layout)
+
+        # Insert car dialog below main window
+        add_car_dialog(self)
 
         #BUTTON ACTIONS
+
         #Quit
-        quit_btn.clicked.connect(QApplication.instance().quit)
+        self.quit_btn.clicked.connect(QApplication.instance().quit)
 
         #List all cars (new window)
-        list_all_cars_btn.clicked.connect(self.switch)
+        self.list_all_cars_btn.clicked.connect(self.switch)
 
-        # #Add car dialog
-        # add_car_btn.clicked.connect(self.add_car_clicked)
-        # self.setGeometry(300, 300, 350, 300)
-        # self.setWindowTitle('Main menu - Vehicle manager')
-        # self.show()
+        #Show add car dialog
+        self.add_car_btn.clicked.connect(show_add_car_dialog)
 
-    # def list_cars_clicked(self):
-    #     self.sw = ViewCars()
-    #     self.sw.setGeometry(700, 300, 270, 300)
-    #     self.sw.setWindowTitle('List of cars in file')
-    #     self.sw.show()
-    def add_car_clicked(self):
-        self.tw = AddCar()
-        self.tw.show()
+        #Add new car when clicking submit
+
 
     def switch(self):
         self.switch_window.emit()
@@ -80,7 +184,7 @@ class ViewCars(QWidget):
         super(QWidget, self).__init__()
 
         self.setWindowTitle("List of cars in file - Vehicle manager")
-        self.setGeometry(300, 300, 400, 300)
+        self.setGeometry(1000, 30, 450, 800)
 
         # Scrolling widget
         widget = QWidget()
@@ -132,30 +236,20 @@ class AddCar(QWidget):
 
     def __init__(self):
         super(AddCar, self).__init__()
-        self.setFixedHeight(200)
+        self.setFixedHeight(100)
 
-        # Container Widget
-        widget = QWidget()
-        # Layout of Container Widget
-        layout = QVBoxLayout(self)
-
-        # for _ in range(20):
-        #     btn = QPushButton('test')
-        #     layout.addWidget(btn)
-
-        widget.setLayout(layout)
-
-        # Scroll Area Properties
-        scroll = QScrollArea()
-        scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
-        scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        scroll.setWidgetResizable(False)
-        scroll.setWidget(widget)
-
-        # Scroll Area Layer add
-        vLayout = QVBoxLayout(self)
-        vLayout.addWidget(scroll)
-        self.setLayout(vLayout)
+        # # Container Widget
+        # widget = QWidget()
+        # # Layout of Container Widget
+        # layout = QVBoxLayout(self)
+        #
+        # # for _ in range(20):
+        # #     btn = QPushButton('test')
+        # #     layout.addWidget(btn)
+        #
+        # widget.setLayout(layout)
+        #
+        # self.setLayout(layout)
 
 class Controller:
     def __init__(self):
@@ -173,10 +267,15 @@ class Controller:
         self.window_car_list.show()
 
 
+def main():
+    app = QApplication(sys.argv)
+    app.setFont(QFont('SansSerif', 20))
+    controller = Controller()
+    controller.show_main()
+    sys.exit(app.exec_())
 
-app = QApplication(sys.argv)
-controller = Controller()
-controller.show_main()
-sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
 
 
